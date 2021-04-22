e2cbdd886ccecce5e077cee8686381cbe24975ff
Cem <ce5368al-s@student.lu.se>
2021-04-22 19:49:24 +0200

    Added custom reward and termination functions

--- src/Training_Double_Pendulum.py
+++ src/Training_Double_Pendulum.py
@@ -23,33 +23,50 @@ if len(sys.argv) > 1:
     else:
         print("usage: Training_Pendulum.py --warm")
 
+max_angle = 10*np.pi/180
+
+def reward(state, t):
+    x, theta1, theta2, xdot, theta1dot, thteta2dot = state
+    r_angle2 = (max_angle - abs(theta2))/max_angle
+    return r_angle2
+
+def terminated(state, t):
+    x, theta1, theta2, xdot, theta1dot, thteta2dot = state
+
+    return abs(theta1) > max_angle or abs(theta2) > max_angle
 
 # Setup the environment (connects the problem to the q-agent).
-step_size = 0.01
-environment = DoublePendulumOnCartEnvironment(step_size)
+step_size = 0.02
+environment = DoublePendulumOnCartEnvironment(
+        step_size=step_size,
+        custom_reward_function=reward,
+        custom_termination_function=terminated,
+        action_space=[-10,-5,0,5,10])
 
 # Setup Neural network parameters.
 
 optimizer = Adam(lr=0.01)
 network_parameters = {
     "input_shape" : (6,),                                       # Network input shape.
-    "layers" : [(40, 'relu'), (40, 'relu'), (len(environment.get_action_space()), 'linear')],     # [(nodes, activation function)]
+    "layers" : [(100, 'relu'), (100, 'relu'), (len(environment.get_action_space()), 'linear')],     # [(nodes, activation function)]
     "optimizer" : optimizer,                                    # optimizer
     "loss_function" : "mse",                                    # loss function ('mse', etc.)
 }
 
 # Create agent.
-agent = QAgent(environment, network_parameters)
+agent = QAgent(environment, network_parameters, memory=200)
 
 # Train agent - produces a controller that can be used to control the system.
-training_episodes = 3000
-controller = agent.train(max_episodes=training_episodes,warm_start=warm_start)
+controller = agent.train(
+        max_episodes=100,
+        warm_start=warm_start,
+        batch_size=200)
 
 # Simulate problem using the trained controller.
-max_time_steps = 100
 state = environment.reset()
 
-t = np.linspace(0, 10, 100)
+t = np.linspace(0, 10, 500)
 environment.solve(t, controller=controller.act)
 
 environment.animate()
+environment.problem.animate(save=True,filename="result.gif")

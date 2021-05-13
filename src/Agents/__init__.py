61a56aa647d7c38d3d63a09976d50a158b055b06
Cem <ce5368al-s@student.lu.se>
2021-05-13 21:29:55 +0200

    Added DoubleQAgent

--- src/Agents/__init__.py
+++ src/Agents/__init__.py
@@ -1,3 +1,5 @@
 from .Controller import Controller
 from .NetworkBuilder import NetworkBuilder
 from .QAgent import QAgent
+from .LinearAgent import LinearAgent
+from .DoubleQAgent import DoubleQAgent

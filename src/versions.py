0bede49cc78972206c3d246a2c53f5cd06df4bcc
Pukashawar Pannu <pukashawar_singh.pannu.6218@student.lu.se>
2021-05-31 19:35:33 +0200

    Added python script for listing package versions

---
+++ src/versions.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib as mat
+import pandas as pd
+import scipy as sp
+import sklearn as skl
+import tensorflow as tf
+import pip as p
+
+
+print(f"Numpy Version: {np.__version__}")
+print(f"Scipy Version: {sp.__version__}")
+print(f"Matplotlib Version: {mat.__version__}")
+print(f"Pandas Version: {pd.__version__}")
+print(f"Sklearn Version: {skl.__version__}")
+print(f"Tensorflow Version: {tf.__version__}")
+print(f"Pip version: {p.__version__}")
